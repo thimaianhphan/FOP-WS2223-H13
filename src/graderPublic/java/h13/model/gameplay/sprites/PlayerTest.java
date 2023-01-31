d8b17257688bc2129ada292c315a77a3ff9dee49
Rdeisenroth <rdeisenroth@gmail.com>
2023-01-31 15:01:12 +0100

    Fix PlayerTest

--- src/graderPublic/java/h13/model/gameplay/sprites/PlayerTest.java
+++ src/graderPublic/java/h13/model/gameplay/sprites/PlayerTest.java
@@ -1,14 +1,15 @@
 package h13.model.gameplay.sprites;
 
 import h13.controller.ApplicationSettings;
+import h13.model.gameplay.Direction;
 import h13.model.gameplay.GameState;
 import h13.util.PrettyPrinter;
-import h13.util.StudentLinks;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
 
-import static h13.util.StudentLinks.PlayerLinks.PlayerMethodLink.SHOOT_METHOD;
+import static h13.util.StudentLinks.BattleShipLinks.BattleShipMethodLink.SHOOT_METHOD;
 import static h13.util.StudentLinks.PlayerLinks.PlayerMethodLink.UPDATE_METHOD;
 import static org.mockito.Mockito.*;
 import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.contextBuilder;
@@ -32,9 +33,10 @@ public class PlayerTest {
             .add("elapsedTime", 0)
             .add("requires other methods to work:", "super.update(elapsedTime)")
             .build();
+        SHOOT_METHOD.alwaysDoNothing(context, player);
 
         UPDATE_METHOD.invoke(context, player, 0);
-        SHOOT_METHOD.assertInvokedNTimesWithParams(context, player, 1);
+        SHOOT_METHOD.assertInvokedNTimesWithParams(context, player, 1, Direction.UP);
     }
 
     @Test
@@ -47,7 +49,9 @@ public class PlayerTest {
             .add("requires other methods to work:", "super.update(elapsedTime)")
             .build();
 
+        SHOOT_METHOD.alwaysDoNothing(context, player);
+
         UPDATE_METHOD.invoke(context, player, 0);
-        SHOOT_METHOD.assertNeverInvoked(context, player);
+        SHOOT_METHOD.invoke(context, verify(player, Mockito.never().description(context.toString())), any(Direction.class));
     }
 }
