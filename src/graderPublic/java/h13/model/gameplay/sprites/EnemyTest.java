e7c6c984757853b430c80d2e45d74cc723895fff
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-02-01 14:21:37 +0100

    Improve Enemy test to allow for more solutions (#8)
    * Improve Enemy test to allow for more solutions
    
    * Fixes to the improvement

--- src/graderPublic/java/h13/model/gameplay/sprites/EnemyTest.java
+++ src/graderPublic/java/h13/model/gameplay/sprites/EnemyTest.java
@@ -20,14 +20,14 @@ public class EnemyTest {
     @BeforeEach
     public void setup() {
         ApplicationSettings.loadTexturesProperty().set(false);
+        ApplicationSettings.enemyShootingDelayProperty().set(0);
         // replace Math.random() calls in Enemy with MathRandomTester.random() calls
-        enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
     }
 
     @Test
     public void testUpdateShootCalledWithMaxProbability() {
         ApplicationSettings.enemyShootingProbabilityProperty().set(1);
-        ApplicationSettings.enemyShootingDelayProperty().set(0);
+        enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
         final var context = contextBuilder()
             .add("enemy", PrettyPrinter.prettyPrint(enemy))
             .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
@@ -42,8 +42,7 @@ public class EnemyTest {
     @Test
     public void testUpdateWithMinProbability() {
         ApplicationSettings.enemyShootingProbabilityProperty().set(0);
-        ApplicationSettings.enemyShootingDelayProperty().set(0);
-
+        enemy = spy(new Enemy(0, 0, 0, 0, mock(GameState.class)));
         final var context = contextBuilder()
             .add("enemy", PrettyPrinter.prettyPrint(enemy))
             .add("shootingProbability", ApplicationSettings.enemyShootingProbabilityProperty().get())
